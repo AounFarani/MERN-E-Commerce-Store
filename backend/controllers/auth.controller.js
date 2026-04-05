eac7afd2b93a3c2b28a9953a08d9800ce6bdd035
AounFarani <aounfarani7@gmail.com>
2026-04-06 03:10:30 +0500

    Coupon Routes

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,6 @@
-import User from "../models/user.model.js";
 import jwt from "jsonwebtoken";
+
+import User from "../models/user.model.js";
 import { redis } from "../lib/redis.js";
 
 const generateTokens = (userId) => {
